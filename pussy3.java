73c9cfb64c53e79b6f24105e81afb41aadbba269
Evgeny Gurtovoy <jugin777@gmali.com>
2017-03-01 22:35:35 +0000

    create pussy3

---
+++ pussy3.java
@@ -0,0 +1,10 @@
+
+public class pussy3 {
+	public static void main(String[] args) {
+	System.out.println("HelloWorld");
+	//test1
+	//test2
+	//test3
+	System.out.println("dog3 ...+cat+dog");
+	}
+}
